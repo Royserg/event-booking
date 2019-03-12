7c895f363d2d974de2991fc9947a475c25a8bc19
Jakub <staniojakub@gmail.com>
2019-03-12 01:20:10 +0100

    Add event model for mongoose

---
+++ models/event.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// create schema
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+
+// create model
+module.exports = mongoose.model('Event', eventSchema);
