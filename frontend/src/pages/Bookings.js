615cd723eecb08e7299628c9a1d71205da94d2d9
Jakub <staniojakub@gmail.com>
2019-07-18 16:56:04 +0200

    Initial frontend components - Navigation

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BookingsPage = props => <h1>Bookings Page</h1>;
+
+export default BookingsPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EventsPage = props => <h1>Events Page</h1>;
+
+export default EventsPage;
