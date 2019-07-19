205af6a81b2e7fe99bfa74b9b1b5439e7f629fdd
Jakub <staniojakub@gmail.com>
2019-07-19 20:06:57 +0200

    Send request to API for login and register

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,5 +1,89 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-const AuthPage = props => <h1>Auth Page</h1>;
+import './Auth.css';
+
+const AuthPage = props => {
+  const emailEl = useRef(null);
+  const passwordEl = useRef(null);
+  const [isLogin, setIsLogin] = useState(true);
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const email = emailEl.current.value;
+    const password = passwordEl.current.value;
+
+    // Validation
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+
+      `
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(email: "${email}", password: "${password}") {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    fetch('http://localhost:5000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form className='auth-form' onSubmit={onFormSubmit}>
+      <div className='form-control'>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' ref={emailEl} />
+      </div>
+
+      <div className='form-control'>
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' ref={passwordEl} />
+      </div>
+
+      <div className='form-actions'>
+        <button type='submit'>Submit</button>
+        <button type='button' onClick={() => setIsLogin(!isLogin)}>
+          Switch to {isLogin ? 'Signup' : 'Login'}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default AuthPage;
