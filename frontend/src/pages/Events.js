4c5dafcf814b0e6137aa25570e75775da87fc962
Jakub <staniojakub@gmail.com>
2019-07-23 01:04:40 +0200

    Add modal and back drop with basic functionality

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Modal from 'components/Modal/Modal';
+import Backdrop from 'components/Backdrop/Backdrop';
 import './Events.css';
 
-const EventsPage = props => (
-  <div className='events-control'>
-    <p>Share your own events</p>
-    <button className='btn'>Create Event</button>
-  </div>
-);
+const EventsPage = props => {
+  const [creating, setCreating] = useState(false);
+
+  const modalCancelHandler = () => {
+    setCreating(false);
+  };
+  const modalConfirmHandler = () => {
+    setCreating(false);
+  };
+
+  return (
+    <React.Fragment>
+      {creating && (
+        <React.Fragment>
+          <Backdrop />}
+          <Modal
+            title='Add Event'
+            canCancel
+            canConfirm
+            onCancel={modalCancelHandler}
+            onConfirm={modalConfirmHandler}
+          >
+            <p>Modal Contetnt</p>
+          </Modal>
+        </React.Fragment>
+      )}
+      <div className='events-control'>
+        <p>Share your own events</p>
+        <button className='btn' onClick={() => setCreating(true)}>
+          Create Event
+        </button>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default EventsPage;
